7aaee366d948d40e75eb83e2c038a1519d5d1dcc
hainq <nguyenquanghaik53cb@gmail.com>
2012-10-17 14:29:47 +0700

    commit to hainq.git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demoproject::Application.config.secret_token = '4ac8f75efcf7d8d3be9ae9d5b66da35d215a0fb718b1259a14006c0b400d9a9f905c3c5fd5619750631b7d2cb99a224d1b8f87c9278566095e8108f825c8d44c'
